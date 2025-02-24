def3c9f7bf2300395a060fd44fe1127f68d198ac
koenraad@khd.be <koenraad@khd.be>
2025-02-24 08:04:50 +0100

    add machine

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,12 +1,13 @@
 
 # Sending statistics to the Azure Storage account tablen no PII inforfamtion will be send. If you do not wish to send statistics, please comment out this line.
 
+
 $webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
 
 $deploymentData = @{
     Deployment = "azd-devpc"
     User = $env:USERNAME
-    Machine = $env:COMPUTERNAME
+    Machine = $env:AZUREPS_HOST_ENVIRONMENT
     CommitHash = (git rev-parse HEAD)
     AzureUser = $(az account show --query user.name -o tsv)
 } | ConvertTo-Json -Depth 10
