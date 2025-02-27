bdcaa05a0879276273c14924280507c0872c0a8b
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-02-27 09:31:18 +0100

    Update postprovision.ps1

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,4 +1,5 @@
 azd env get-values > .env
+$environmentName = $env:AZURE_ENV_NAME
 # Sending statistics to the Azure Storage account tablen no PII inforfamtion will be send. If you do not wish to send statistics, please comment out this line.
 
 
@@ -6,6 +7,7 @@ $webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-aut
 
 $deploymentData = @{
     Deployment = "AZD-WIN11-DEV-PC-DOCKER"
+    environmentName = $environmentName
     Machine = $env:AZUREPS_HOST_ENVIRONMENT
     CommitHash = (git rev-parse HEAD)
   } | ConvertTo-Json -Depth 10
