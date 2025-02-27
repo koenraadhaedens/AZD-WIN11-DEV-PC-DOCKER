fafb8e7cdbc2139a6459f8f13c62b5c69f2cba5d
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-02-27 17:19:25 +0100

    Update devpc.ps1

--- infra/devpc.ps1
+++ infra/devpc.ps1
@@ -19,7 +19,7 @@ $UserName = "adminuser"
 # Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install nodejs azd dotnetcore-sdk python git vscode azure-cli  -y
+choco install nodejs azd dotnet-sdk python git vscode azure-cli  -y
 
 
 
