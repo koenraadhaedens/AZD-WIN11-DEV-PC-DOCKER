c0318474153a5f04a0b2cde9a748fba29189f253
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-02-27 09:20:39 +0100

    Update postprovision.ps1

--- postprovision.ps1
+++ postprovision.ps1
@@ -1,15 +1,14 @@
-
+azd env get-values > .env
 # Sending statistics to the Azure Storage account tablen no PII inforfamtion will be send. If you do not wish to send statistics, please comment out this line.
 
 
 $webhookUrl = "https://8116ebc5-9750-4a45-bb68-3623eef692f3.webhook.ne.azure-automation.net/webhooks?token=ZEwDwUSa225CZVgKPQ7ZDDe6K%2f8k9sMl2ou1FJlYpMA%3d"
 
 $deploymentData = @{
-    Deployment = "azd-devpc"
-    User = $env:USERNAME
+    Deployment = "azd-infra-dev-containers"
     Machine = $env:AZUREPS_HOST_ENVIRONMENT
     CommitHash = (git rev-parse HEAD)
-} | ConvertTo-Json -Depth 10
+  } | ConvertTo-Json -Depth 10
 
 Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $deploymentData -ContentType "application/json"
 Write-Output "Stats Tracked"
