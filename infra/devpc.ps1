5c79c11cc47cc96965b276f26ffb1053165cc117
Koenraad Haedens <40601026+koenraadhaedens@users.noreply.github.com>
2025-05-13 15:40:26 +0200

    Update devpc.ps1

--- infra/devpc.ps1
+++ infra/devpc.ps1
@@ -1,6 +1,8 @@
 $UserName = "adminuser"
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install nodejs azd dotnet-sdk python git vscode azure-cli  -y
+choco install azd dotnet-sdk python git vscode azure-cli  -y
+
 $dockerUrl = "https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe"
 $installerPath = "$env:TEMP\DockerDesktopInstaller1.exe"
 Start-BitsTransfer -Source $dockerUrl -Destination $installerPath
